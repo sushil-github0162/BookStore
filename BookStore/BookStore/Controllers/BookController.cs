684445258dd6dcf0831faf65701bbffde7894fcb
sushil-github0162 <sushilksagar@gmail.com>
2021-07-16 22:42:48 +0530

    Book Controller Added

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetBookName()
+        {
+            return "Books Index";
+        }
+
+        public string GetBooksById(int id)
+        {
+            return "Book" + id;
+        }
+
+        public string SearchBook(string strBookname, string strAuthorName)
+        {
+            return $"Boom with Name= {strBookname} and Author = {strAuthorName}";
+        }
+    }
+}
