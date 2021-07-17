d80af1fc6a163b69162e6b9ab29d1918304595a6
sushil-github0162 <sushilksagar@gmail.com>
2021-07-17 09:00:27 +0530

    Static files

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -26,7 +26,7 @@ namespace BookStore
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
